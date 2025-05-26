8ce6e3a08b8ba9921a66c63aa08e32ee366498ae
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2025-05-26 18:18:10 -0400

    build(lint): remove extra curlies

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
@@ -15,10 +16,15 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
+      react: react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
+      "react/jsx-curly-brace-presence": [
+        "error",
+        { props: "never", children: "never" },
+      ],
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": [
         "warn",
